172a5a53142eac8bc57b3b9e697539ea3c14983b
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2021-09-23 14:15:04 +0200

    read token

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
+import net.dv8tion.jda.api.JDABuilder
+import scala.io.Source
+
 object Main {
+
   def main(args: Array[String]): Unit = {
-    println("Hello, world")
+    val token = readToken()
+    val jda = JDABuilder.createDefault(token).build
   }
+
+  def readToken() = Source.fromFile("token.txt").getLines.toList.mkString
 }
